13b95e39125c0ea1dd904f008399933fb33024c7
rytantula <rytisss@gmail.com>
2022-05-28 16:38:36 +0100

    Add game function

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ function computerPlay() {
     switch (result) {
         case 1:
             return 'Rock';
-            break;
         case 2:
             return 'Paper';
-            break;
         case 3:
             return 'Scissors';
-            break;
-        default:
-            break;
     }
 }
 
- function playRound (playerSelection, computerSelection) {
+ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
@@ -38,5 +33,36 @@ function computerPlay() {
         } else if (computerSelection == 'paper') {
             return "You Win! Scissors beats Paper";
         }
+    }
+}
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    const answer = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    for (let i = 0; i < 5; i++) {
+        if (answer === 'rock' || answer === 'paper' || answer === 'scissors') {
+            alert(`Round ${[i + 1]}`);
+            const thisGame = playRound(answer, computerPlay());
+            if (thisGame.toString().includes('Win')) {
+                playerScore++;
+                alert (`Player: ${playerScore}, Computer: ${computerScore}`);
+            } else if (thisGame.toString().includes('Lose')) {
+                computerScore++;
+                alert (`Player: ${playerScore}, Computer: ${computerScore}`);
+            } else {
+                alert (`Player: ${playerScore}, Computer: ${computerScore}`);
+            }   
+        } else {
+            return "Please choose either Rock, Paper, or Scissors";
+        }
+    } 
+        if (playerScore > computerScore) {
+            return (`The game is over! You've won`);
+    } else if (playerScore < computerScore) {
+        return (`The game is over! You've lost`);
+    } else {
+        return (`The game is over! It's a tie`);
     }
 }
